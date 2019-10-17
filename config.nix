178c0e016fd9163141795abe5ff00bf2e70c5415
David Meister <thedavidmeister@gmail.com>
2019-10-17 15:18:25 +1100

    release prep

--- config.nix
+++ config.nix
@@ -87,58 +87,6 @@ hl-release-hook-publish
    # - {{ changelog }} will inject the changelog as at the target commit
    template = ''
    {{ changelog }}
-
-   # Installation
-
-   This release includes binaries of:
-
-   - the [`hc` development command-line tool](https://github.com/holochain/holochain-rust/blob/${tag}/cli/README.md)
-   - [`holochain` deployment conductor](https://github.com/holochain/holochain-rust/blob/${tag}/conductor/README.md) for different platforms.
-
-   ## Very much recommended install
-
-   The recommended installation process is to follow the [developer quick-start guide](https://developer.holochain.org/start.html).
-
-   The approach in the quick start guide:
-
-   - provides additional supporting tools like rust & node
-   - shows you how to keep up to date with the latest versions of everything
-   - makes minimal changes to your machine overall
-   - is relatively difficult to screw up
-
-   ## Bothersome manual install
-
-   **IMPORTANT:** Manual holochain installations can conflict with the installer.
-
-   Either binary is installed by being placed anywhere on your `$PATH`.
-   This is different for everyone and depends how your machine is configured.
-
-   For `hc` to build and test DNA Rust and NodeJS are both needed.
-
-   ### Which Rust?
-
-   The binaries for this release were built with rust from holonix version ${holonix-version}.
-   WASM needs the `wasm32-unknown-unknown` rust target on your toolchain.
-
-   ### Which NodeJS?
-
-   Node is used to run end to end tests as a client of the holochain.
-   Holochain exposes websockets for node to interact with.
-
-   We recommend nodejs 10+.
-
-   ### Which Binary?
-
-   Download the binaries for your operating system.
-
-   - MacOS: `cli-${tag}-x86_64-apple-darwin.tar.gz`
-   - Linux: `cli-${tag}-x86_64-generic-linux-gnu.tar.gz`
-   - Windows:
-     - Visual Studio build system (default): `cli-${tag}-x86_64-pc-windows-msvc.tar.gz`
-     - mingw build system: `cli-${tag}-x86_64-pc-windows-gnu.tar.gz`
-
-   All binaries are for 64-bit operating systems.
-   32-bit systems are NOT supported.
 '';
 
    # owner of the github repository that release are deployed to
